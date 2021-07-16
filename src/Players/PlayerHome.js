f3f8a7312821da0f3c08c0100af960bd3c143810
arthurleal98 <arthurjfleal@gmail.com>
2021-07-16 17:11:53 -0300

    remoção de bug ao digitar sem apertar o botão de busca

--- src/Players/PlayerHome.js
+++ src/Players/PlayerHome.js
@@ -16,7 +16,7 @@ const HomePlayer = ()=>{
         <div style={styleForm}>
             <form className="d-flex " onSubmit={teste}>
                   <input className="form-control me-1" type="search" placeholder="Nome do Jogador" aria-label="Search" onChange={e=>setValue(e.target.value)} onSubmit={teste}></input>
-                  <Link to={`/Player/${value}`}><button className="btn btn-outline-success" type="submit" >Search</button></Link>
+                  <button className="btn btn-outline-success" type="submit" ><Link to={`/Player/${value}`}>Search</Link></button>
                 </form>
         </div>
     )
